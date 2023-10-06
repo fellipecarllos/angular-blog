cc09151e97469b161ec00ee86346498332e0e954
Felipe Carlos <fellipecarllos@msn.com>
2023-10-06 00:35:11 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+  {
+     "id":1,
+     "title":"Minha noticia",
+     "description": "Teste",
+     "photoCover": "https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png"
+  },
+
+  {
+    "id":2,
+    "title":"Teste1",
+    "description": "Teste01",
+    "photoCover": "https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png"
+ }
+ {
+  "id":3,
+  "title":"Teste1",
+  "description": "Teste02",
+  "photoCover": "https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png"
+}
+{
+  "id":4,
+  "title":"Teste1",
+  "description": "Teste03",
+  "photoCover": "https://blog.iprocess.com.br/wp-content/uploads/2021/11/placeholder.png"
+}
+]
